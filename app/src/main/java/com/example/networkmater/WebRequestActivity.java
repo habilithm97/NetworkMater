d982d7a2a4719e4590e4e0a4bd6c7e309be9628a
habilithm97 <habinkim97@gmail.com>
2022-02-03 01:02:57 +0900

    Volley 실습

--- app/src/main/java/com/example/networkmater/WebRequestActivity.java
+++ app/src/main/java/com/example/networkmater/WebRequestActivity.java
@@ -10,21 +10,48 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+/*
+*HttpURLConnection 객체를 사용하는 것은 요청과 응답에 필요한 코드의 양이 많음(스레드를 사용하면서 넣어야하는 코드 양도 많음)
+*Volley 라이브러리 : 웹 요청과 응답을 단순화하기 위한 라이브러리
+*Volley 사용법 : 요청 객체 생성 후 요청 큐에 넣으면 됨 -> 요청 큐가 알아서 웹서버에 요청하고 응답까지 받아줌
+
+*Volley의 가장 큰 장점 : 스레드를 신경 쓰지 않아도 됨 -> 메인 스레드에서 UI에 접근할 수 있도록 알아서 해줌(응답 받으면 바로 UI 업데이트)
+ */
 
 public class WebRequestActivity extends AppCompatActivity {
 
     Handler handler = new Handler();
 
     TextView tv;
+    EditText edt2;
+
+    static RequestQueue requestQueue; // 요청 큐 -> 한번만 만들어서 계속 사용함
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_request);
+
+        edt2 = (EditText)findViewById(R.id.edt2);
+        Button btn2 = (Button)findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                volleyRequest();
+            }
+        });
+
+        if(requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(getApplicationContext()); // 요청 큐 객체 생성(여러번 생성 되지 않게 null일 경우만 생성)
+        }
 
         EditText edt = (EditText)findViewById(R.id.edt);
         Button btn = (Button)findViewById(R.id.btn);
@@ -45,6 +72,10 @@ public class WebRequestActivity extends AppCompatActivity {
             }
         });
         tv = (TextView)findViewById(R.id.tv);
+    }
+
+    public void volleyRequest() {
+        String url = edt2.
     }
 
     public void request(String urlStr) {
